54a6e7f55aaeca79532081eba4959b15a0092e28
Alexis Lavenu <alexis.lavenu@epitech.eu>
2018-03-15 14:38:25 +0100

    Add Main Menu

---
+++ EIP_TestTwitchIntegration/Assets/Scripts/VersionTextScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class VersionTextScript : MonoBehaviour {
+
+	public Text             _version;
+
+	// Use this for initialization
+	void Start () {
+		//string text = System.IO.File.ReadAllText(@"C:\Users\Public\TestFolder\WriteText.txt");
+		string text = System.IO.File.ReadAllText("Assets/Files/version.txt");
+		//_version.text = "TEST";
+		_version.text = text;
+	}
+}
